858c56bf72bdd88a262ad0fa0956f704584bd7ca
qiucanlei <772904865@qq.com>
2022-10-21 11:44:01 +0800

    feat：添加搜索组件

---
+++ src/components/SearchNote.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="search">
+    <input type="text" placeholder="搜索笔记">
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+  .search {
+    margin-top: 24px;
+    width: 100%;
+  }
+</style>>
