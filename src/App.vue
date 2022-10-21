6ef23122d3df8e83bc6c810fc859a8ae30c878e4
qiucanlei <772904865@qq.com>
2022-10-21 11:43:03 +0800

    feat: 首页布局

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,25 @@
 <script setup>
-
+import SearchNote from "./components/SearchNote.vue";
+import NoteList from "./components/NoteList.vue";
+import AddNote from "./components/AddNote.vue";
 </script>
 
 <template>
-  
+  <main>
+    <h1>我的笔记本</h1>
+    <SearchNote />
+    <NoteList />
+    <AddNote />
+  </main>
 </template>
 
 <style scoped>
-
+main {
+  width: 600px;
+  margin: 0 auto;
+  height: 100%;
+  display: grid;
+  justify-items: center;
+  padding: 48px 0;
+}
 </style>
