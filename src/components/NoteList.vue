63f94246246e93d1113d01b4e6dad6a3a372ab5b
qiucanlei <772904865@qq.com>
2022-10-21 11:43:37 +0800

    feat：添加列表组件

---
+++ src/components/NoteList.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="noteList">
+    <div v-for="n in 6" class="note" :key="n">
+      <h2>笔记标题{{ n }}</h2>
+      <article>
+        <p>这是笔记内容 {{ n }}</p>
+      </article>
+    </div>
+  </div>
+</template>
+<script setup></script>
+<style scoped>
+.noteList {
+  margin: 48px 0;
+  display: grid;
+  row-gap: 48px;
+  width: 100%;
+}
+
+.note h2 {
+  font-weight: normal;
+  margin-bottom: 12px;
+}
+
+.note article {
+  color: hsl(0deg, 0%, 40%);
+}
+</style>
